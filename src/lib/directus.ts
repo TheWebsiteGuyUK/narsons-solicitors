a763486a400a0c8e738d00e10d442802f4d3b322
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2024-08-09 21:56:13 +0100

    wip

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -3,33 +3,39 @@ import { createDirectus, rest } from '@directus/sdk';
 type Global = {
   title: string;
   description: string;
+  logo: string;
 }
 
 type HomePage = {
   title: string;
-  content: string | null;
-  slug: string;
-  image: string | null;
-  heroImage: string | null;
-  heroImageAlt: string | null;
-  heroHeadline: string;
-  heroContent: string;
-  heroButtons: { label: string; href: string }[];
-  Services: string[]; // Array of service IDs
-}
-
-type Services = {
-  id: number;
-  headline: string;
   content: string;
   slug: string;
   image: string;
+  heroImage: string;
+  heroImageAlt: string;
+  heroHeadline: string;
+  heroContent: string;
+  heroButtons: { label: string; href: string }[];
+
+}
+
+type Services = {
+  title: string;
+  content: string;
+  slug: string;
+  image: string;
+  heroImage: string;
+  heroImageAlt: string;
+  heroHeadline: string;
+  heroContent: string;
+  heroButtons: { label: string; href: string }[];
+
 }
 
 type Schema = {
   global: Global;
-  services: Services[];
   home_page: HomePage;
+  services: Services;
 }
 
 // Initialize Directus instance
---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          50: '#E3E8F0', 
+          100: '#C6CEDD', 
+          200: '#95A5C5', 
+          300: '#657DAF', 
+          400: '#345599', 
+          500: '#0D2744', 
+          600: '#0C233D', 
+          700: '#0A1F36', 
+          800: '#091A2F', 
+          900: '#071628', 
+        },
+        'silver': '#B5B5B5', 
+      },
+    },
+  },
+  plugins: [require("preline/plugin")],
+};
